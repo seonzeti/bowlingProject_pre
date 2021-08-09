c9d8353abc82b42f0ededd76c505f974218939b2
seonzeti <seonzeroticket@gmail.com>
2021-08-09 17:00:11 +0900

    - DB Commit

--- src/test/java/com/comin/bowling/maria/MyBatisTest.java
+++ src/test/java/com/comin/bowling/maria/MyBatisTest.java
@@ -1,32 +1,38 @@
 package com.comin.bowling.maria;
 
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+
 public class MyBatisTest {
+	private static final Logger logger = LoggerFactory.getLogger(MyBatisTest.class);
 	
-	@Inject
-	private SqlSessionFactory sqlFactory;
+	@Inject private SqlSessionFactory sqlFactory;
 	
 	@Test
-	public void tesstFactory() {
-		System.out.println("sqlFactory : " + sqlFactory);
+	public void testFactory() {
+		logger.info("sqlFactory:" + sqlFactory);
 	}
 	
 	@Test
-	public void testSession() throws Exception {
-		try(SqlSession session = sqlFactory.openSession()){
-			System.out.println("session : " + session);
-		}catch (Exception e) {
+	public void testSession() {
+		try(SqlSession sqlSession = sqlFactory.openSession()) {
+			logger.info("sqlSession : " + sqlSession);
+			logger.info("mybatis 연결 성공");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 }
