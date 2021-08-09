c4064bee0ca966c30c345eef82af9d3c0bebfbbe
seonzeti <seonzeroticket@gmail.com>
2021-08-09 17:18:30 +0900

    - db connection (version updated)

--- src/test/java/com/comin/bowling/maria/DBConnectionTest.java
+++ src/test/java/com/comin/bowling/maria/DBConnectionTest.java
@@ -9,7 +9,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+
+@WebAppConfiguration 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
 public class DBConnectionTest {
--- src/test/java/com/comin/bowling/maria/MyBatisTest.java
+++ src/test/java/com/comin/bowling/maria/MyBatisTest.java
@@ -3,6 +3,7 @@ package com.comin.bowling.maria;
 
 import javax.inject.Inject;
 
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
@@ -11,7 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@WebAppConfiguration 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 
